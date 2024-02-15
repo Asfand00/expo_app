c0e7b15bebded1dca625d40b2a8ecdcedfbb2525
Asfandyar Tanwer <asfandtanwer@yahoo.com>
2024-02-14 18:45:13 -0800

    Initial Commit

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello This is my expo app, my name is Asfand</Text>
       <StatusBar style="auto" />
     </View>
   );
